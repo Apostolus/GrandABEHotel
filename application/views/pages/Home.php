cb17612cbe1162e2a9e670739b97d9613d2ed9e2
FURIBAITO <furibaito@gmail.com>
2016-09-04 15:45:26 +0700

    Navigation Bar Improvement
    Plus using SCSS for better modularity and integrity.

--- application/views/pages/Home.php
+++ application/views/pages/Home.php
@@ -1,236 +1,240 @@
 <div class="container-fluid">
-  <br>
-  <!-- CAROUSEL -->
-  <div id="imgCarousel" class="carousel slide" data-ride="carousel">
-    <!--Indicator-->
-    <ol class="carousel-indicators">
-      <li data-target="#imgCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#imgCarousel" data-slide-to="1"></li>
-      <li data-target="#imgCarousel" data-slide-to="2"></li>
-      <li data-target="#imgCarousel" data-slide-to="3"></li>
-    </ol>
+	<br>
+	<!-- CAROUSEL -->
+	<div id="imgCarousel" class="carousel slide" data-ride="carousel">
+		<!--Indicator-->
+		<ol class="carousel-indicators">
+			<li data-target="#imgCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#imgCarousel" data-slide-to="1"></li>
+			<li data-target="#imgCarousel" data-slide-to="2"></li>
+			<li data-target="#imgCarousel" data-slide-to="3"></li>
+		</ol>
 
-    <!-- Wrapper-->
-    <div class="carousel-inner" role="listbox">
+		<!-- Wrapper-->
+		<div class="carousel-inner" role="listbox">
 
-      <div class="item active">
-        <img src="assets/img/home1.jpg" alt="Room 1" width="100%">
-        <div class="carousel-caption">
-          <h3>Room 1</h3>
-          <img src="assets/img/line.svg" width="50%" style="margin:2vw" class="hidden-xs">
-          <p class="hidden-xs">Lorem Ipsum </p>
-        </div>
-      </div>
+			<div class="item active">
+				<img src="assets/img/home1.jpg" alt="Room 1" width="100%">
+				<div class="carousel-image-mask"></div>
+				<div class="carousel-caption">
+					<h3>Room 1</h3>
+					<img src="assets/img/line.svg" width="50%" style="margin:2vw" class="hidden-xs">
+				<p class="hidden-xs">Lorem Ipsum </p>
+				</div>
+			</div>
 
-      <div class="item">
-        <img src="assets/img/home2.jpg" alt="Room 2" width="100%">
-        <div class="carousel-caption">
-          <h3>Room 2</h3>
-          <img src="assets/img/line.svg" width="50%" style="margin:2vw" class="hidden-xs">
-          <p class="hidden-xs">Ipsum Lorem</p>
-        </div>
-      </div>
-    
-      <div class="item">
-        <img src="assets/img/home3.jpg" alt="Room 3" width="100%">
-        <div class="carousel-caption">
-          <h3>Room 3</h3>
-          <img src="assets/img/line.svg" width="50%" style="margin:2vw" class="hidden-xs">
-          <p class="hidden-xs">Lorem Ipsum</p>
-        </div>
-      </div>
+			<div class="item">
+				<img src="assets/img/home2.jpg" alt="Room 2" width="100%">
+				<div class="carousel-image-mask"></div>
+				<div class="carousel-caption">
+				<h3>Room 2</h3>
+				<img src="assets/img/line.svg" width="50%" style="margin:2vw" class="hidden-xs">
+				<p class="hidden-xs">Ipsum Lorem</p>
+				</div>
+			</div>
+			
+			<div class="item">
+				<img src="assets/img/home3.jpg" alt="Room 3" width="100%">
+				<div class="carousel-image-mask"></div>
+				<div class="carousel-caption">
+				<h3>Room 3</h3>
+				<img src="assets/img/line.svg" width="50%" style="margin:2vw" class="hidden-xs">
+				<p class="hidden-xs">Lorem Ipsum</p>
+				</div>
+			</div>
 
-      <div class="item">
-        <img src="assets/img/home4.jpg" alt="Room 4" width="100%">
-        <div class="carousel-caption">
-          <h3>Room 4</h3>
-          <img src="assets/img/line.svg" width="50%" style="margin:2vw" class="hidden-xs">
-          <p class="hidden-xs">Lorem Ipsum</p>
-        </div>
-      </div>
-  
-    </div>
+			<div class="item">
+				<img src="assets/img/home4.jpg" alt="Room 4" width="100%">
+				<div class="carousel-image-mask"></div>
+				<div class="carousel-caption">
+				<h3>Room 4</h3>
+				<img src="assets/img/line.svg" width="50%" style="margin:2vw" class="hidden-xs">
+				<p class="hidden-xs">Lorem Ipsum</p>
+				</div>
+			</div>
 
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#imgCarousel" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true" style="color:#ddd395"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#imgCarousel" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true" style="color:#ddd395"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
+		</div>
 
-  <!-- BOOKING -->
-  <div class="bookingDiv">
-    <center>
-    <!-- for MD &LG -->
-    <div class="row visible-md visible-lg">
-      <div class="col-xs-12">
-        <form>
-          <table style="width:60%; color:#ddd395;">
-            <tr>
-              <td>Check-In</td>
-              <td>Check-Out</td>
-              <td>Adults</td>
-              <td>Children</td>
-              <td>Promo Code</td>
-            </tr>
-            <tr>
-              <td><input type="date" id="checkin"></td>
-              <td><input type="date" id="checkout"></td>
-              <td><input type="text" id="adult" onkeypress='return event.charCode >= 48 && event.charCode <= 57' placeholder="numbers only"></td>
-              <td><input type="text" id="children" onkeypress='return event.charCode >= 48 && event.charCode <= 57' placeholder="numbers only"></td>
-              <td><input type="text" id="promoCode"></td>
-              <td><input type="submit" value="Book" class="button"></td>
-            </tr>
-          </table>
-        </form>
-      </div>
-    </div>
-       
-    <!-- for XS & SM -->
-    <div class="row visible-xs visible-sm" style="width:65%">
-      <div class="col-xs-12">
-        <form style=" color:#ddd395">     
-          <div class="form-group">
-            <label for="checkin">Check-In:</label>
-            <input type="date" class="form-control" id="checkin">
-          </div>
-          <div class="form-group">
-            <label for="checkout">Check-Out:</label>
-            <input type="date" class="form-control" id="checkout">
-          </div>
-          <div class="form-group">
-            <label for="adult">Adults:</label>
-            <input type="text" class="form-control" id="adult" onkeypress='return event.charCode >= 48 && event.charCode <= 57' placeholder="numbers only">
-          </div>
-          <div class="form-group">
-            <label for="adult">Children:</label>
-            <input type="text" class="form-control" id="children" onkeypress='return event.charCode >= 48 && event.charCode <= 57' placeholder="numbers only">
-          </div>
-          <div class="form-group">
-            <label for="promoCode">Promo Code:</label>
-            <input type="text" class="form-control" id="promoCode">
-          </div>
-          <br>
-          <input type="submit" value="Book" class="button">
-        </form>
-      </div>
-    </div>
-      
-    </center>
-  </div>
+		<!-- Left and right controls -->
+		<a class="left carousel-control" href="#imgCarousel" role="button" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true" style="color:#ddd395"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#imgCarousel" role="button" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true" style="color:#ddd395"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div>
 
-  <!-- FACILITIES DESC -->
-  <div class="facilitiesDiv">
-    <center>
-      <div class="row" style="width:75%">
-        <a href="#">
-        <div class="col-xs-12 col-md-4">
-          <div class="col-xs-12 facilityDesc"><img src="assets/img/placeholderIcon.png" width="50vh"></div>
-          <div class="col-xs-12 facilityDesc"><img src="assets/img/line.svg" width="75%"></div>
-          <div class="col-xs-12 facilityDesc"><h3 style="color:#ddd395;">SOMETHING</h3></div>
-          <div class="col-xs-12 facilityDesc"><p style="color:white;">TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT</p></div>          
-        </div>
-       </a>
-          <hr class="col-xs-12 visible-xs visible-sm">
+	<!-- BOOKING -->
+	<div class="bookingDiv">
+		<center>
+			<!-- for MD &LG -->
+			<div class="row visible-md visible-lg">
+				<div class="col-xs-12">
+					<form>
+						<!--<table style="width:60%; color:#ddd395;">
+							<tr>
+							<td>Check-In</td>
+							<td>Check-Out</td>
+							<td>Adults</td>
+							<td>Children</td>
+							<td>Promo Code</td>
+							</tr>
+							<tr>
+							<td><input type="date" id="checkin"></td>
+							<td><input type="date" id="checkout"></td>
+							<td><input type="text" id="adult" onkeypress='return event.charCode >= 48 && event.charCode <= 57' placeholder="numbers only"></td>
+							<td><input type="text" id="children" onkeypress='return event.charCode >= 48 && event.charCode <= 57' placeholder="numbers only"></td>
+							<td><input type="text" id="promoCode"></td>
+							<td><input type="submit" value="Book" class="button"></td>
+							</tr>
+						</table>-->
+					</form>
+				</div>
+			</div>
+			
+			<!-- for XS & SM -->
+			<div class="row visible-xs visible-sm" style="width:65%">
+				<div class="col-xs-12">
+					<form style=" color:#ddd395">     
+					<div class="form-group">
+						<label for="checkin">Check-In:</label>
+						<input type="date" class="form-control" id="checkin">
+					</div>
+					<div class="form-group">
+						<label for="checkout">Check-Out:</label>
+						<input type="date" class="form-control" id="checkout">
+					</div>
+					<div class="form-group">
+						<label for="adult">Adults:</label>
+						<input type="text" class="form-control" id="adult" onkeypress='return event.charCode >= 48 && event.charCode <= 57' placeholder="numbers only">
+					</div>
+					<div class="form-group">
+						<label for="adult">Children:</label>
+						<input type="text" class="form-control" id="children" onkeypress='return event.charCode >= 48 && event.charCode <= 57' placeholder="numbers only">
+					</div>
+					<div class="form-group">
+						<label for="promoCode">Promo Code:</label>
+						<input type="text" class="form-control" id="promoCode">
+					</div>
+					<br>
+					<input type="submit" value="Book" class="button">
+					</form>
+				</div>
+			</div>
+		
+		</center>
+	</div>
 
-        <a href="#">
-        <div class="col-xs-12 col-md-4">
-          <div class="col-xs-12 facilityDesc"><img src="assets/img/placeholderIcon.png" width="50vh"></div>
-          <div class="col-xs-12 facilityDesc"><img src="assets/img/line.svg" width="75%"></div>
-          <div class="col-xs-12 facilityDesc"><h3 style="color:#ddd395;">SOMETHING</h3></div>
-          <div class="col-xs-12 facilityDesc"><p style="color:white;">TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT</p></div>
-        </div>
-          <hr class="col-xs-12 visible-xs visible-sm">
-        </a>
+	<!-- FACILITIES DESC -->
+	<div class="facilitiesDiv">
+		<center>
+			<div class="row" style="width:75%">
+				<a href="#">
+					<div class="col-xs-12 col-md-4">
+						<div class="col-xs-12 facilityDesc"><img src="assets/img/placeholderIcon.png" width="50vh"></div>
+						<div class="col-xs-12 facilityDesc"><img src="assets/img/line.svg" width="75%"></div>
+						<div class="col-xs-12 facilityDesc"><h3 style="color:#ddd395;">SOMETHING</h3></div>
+						<div class="col-xs-12 facilityDesc"><p style="color:white;">TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT</p></div>          
+					</div>
+				</a>
+				<hr class="col-xs-12 visible-xs visible-sm">
 
-        <a href="#">
-        <div class="col-xs-12 col-md-4">
-          <div class="col-xs-12 facilityDesc"><img src="assets/img/placeholderIcon.png" width="50vh"></div>
-          <div class="col-xs-12 facilityDesc"><img src="assets/img/line.svg" width="75%"></div>
-          <div class="col-xs-12 facilityDesc"><h3 style="color:#ddd395;">SOMETHING</H3></div>
-          <div class="col-xs-12 facilityDesc"><p style="color:white;">TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT</p></div>
-        </div>
-        </a>
-      </div>
-    </center>
-  </div>
+				<a href="#">
+					<div class="col-xs-12 col-md-4">
+						<div class="col-xs-12 facilityDesc"><img src="assets/img/placeholderIcon.png" width="50vh"></div>
+						<div class="col-xs-12 facilityDesc"><img src="assets/img/line.svg" width="75%"></div>
+						<div class="col-xs-12 facilityDesc"><h3 style="color:#ddd395;">SOMETHING</h3></div>
+						<div class="col-xs-12 facilityDesc"><p style="color:white;">TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT</p></div>
+					</div>
+					<hr class="col-xs-12 visible-xs visible-sm">
+				</a>
 
-  <!-- ROOM IMAGES -->
-  <div class="roomImages hidden-xs hidden-sm">
-    <div class="row">
-      <div class="col-md-6 roomImgDiv"><img class="roomImg" src="assets/img/home1.jpg"></div>
-      <div class="col-md-6 roomImgDiv"><img class="roomImg" src="assets/img/home2.jpg"></div>
+				<a href="#">
+					<div class="col-xs-12 col-md-4">
+						<div class="col-xs-12 facilityDesc"><img src="assets/img/placeholderIcon.png" width="50vh"></div>
+						<div class="col-xs-12 facilityDesc"><img src="assets/img/line.svg" width="75%"></div>
+						<div class="col-xs-12 facilityDesc"><h3 style="color:#ddd395;">SOMETHING</H3></div>
+						<div class="col-xs-12 facilityDesc"><p style="color:white;">TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT</p></div>
+					</div>
+				</a>
+			</div>
+		</center>
+	</div>
 
-      <div class="col-md-6 roomImgDiv"><img class="roomImg" src="assets/img/home3.jpg"></div>
-      <div class="col-md-6 roomImgDiv"><img class="roomImg" src="assets/img/home4.jpg"></div>
-    </div>
-  </div>
+	<!-- ROOM IMAGES -->
+	<div class="roomImages hidden-xs hidden-sm">
+		<div class="row">
+			<div class="col-md-6 roomImgDiv"><img class="roomImg" src="assets/img/home1.jpg"></div>
+			<div class="col-md-6 roomImgDiv"><img class="roomImg" src="assets/img/home2.jpg"></div>
 
-  <!-- DESCRIPTIONS -->
-  <div class="roomDetails" style="color: #ddd395;">
-    <br>
-    <br>
-    <center>
-    <div class="row" style="width:75%">
-      <div class="col-md-4"><img src="assets/img/home1.jpg" width="100%"></div>
-      <div class="col-md-8" style="text-align:left">
-        <h3>Lorem Ipsum</h3><br>
-        <p style="color:white;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Fusce mollis, erat in viverra luctus, mi lectus eleifend mauris, et vehicula lacus mi vitae sem. 
-          Phasellus scelerisque mauris ex, sit amet faucibus metus ornare et. Etiam vestibulum odio quis tincidunt aliquet. 
-          Sed sit amet ante at elit tempor maximus. Donec hendrerit vitae libero in rutrum. 
-          Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis a risus libero.</p>
-        <button type="button" class="btn btn-primary btn-md">READ MORE</button>
-      </div>
-    </div>
+			<div class="col-md-6 roomImgDiv"><img class="roomImg" src="assets/img/home3.jpg"></div>
+			<div class="col-md-6 roomImgDiv"><img class="roomImg" src="assets/img/home4.jpg"></div>
+		</div>
+	</div>
 
-    <!-- DIVIDER -->
-    <br>
-    <div class="row">
-      <div class="col-xs-12" style="background-color:#141414; height:2vw"></div>
-    </div>
-    <br>
+	<!-- DESCRIPTIONS -->
+	<div class="roomDetails" style="color: #ddd395;">
+		<br>
+		<br>
+		<center>
+		<div class="row" style="width:75%">
+		<div class="col-md-4"><img src="assets/img/home1.jpg" width="100%"></div>
+		<div class="col-md-8" style="text-align:left">
+			<h3>Lorem Ipsum</h3><br>
+			<p style="color:white;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+			Fusce mollis, erat in viverra luctus, mi lectus eleifend mauris, et vehicula lacus mi vitae sem. 
+			Phasellus scelerisque mauris ex, sit amet faucibus metus ornare et. Etiam vestibulum odio quis tincidunt aliquet. 
+			Sed sit amet ante at elit tempor maximus. Donec hendrerit vitae libero in rutrum. 
+			Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis a risus libero.</p>
+			<button type="button" class="btn btn-primary btn-md">READ MORE</button>
+		</div>
+		</div>
 
-    <div class="row" style="width:75%">
-      <div class="col-md-4"><img src="assets/img/home2.jpg" width="100%"></div>
-      <div class="col-md-8" style="text-align:left">
-        <h3>Lorem Ipsum</h3><br>
-        <p style="color:white;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Fusce mollis, erat in viverra luctus, mi lectus eleifend mauris, et vehicula lacus mi vitae sem. 
-          Phasellus scelerisque mauris ex, sit amet faucibus metus ornare et. Etiam vestibulum odio quis tincidunt aliquet. 
-          Sed sit amet ante at elit tempor maximus. Donec hendrerit vitae libero in rutrum. 
-          Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis a risus libero.</p>
-        <button type="button" class="btn btn-primary btn-md">READ MORE</button>
-      </div>
-    </div>
-    </center>
-    <br>
-    <br>
-  </div>
+		<!-- DIVIDER -->
+		<br>
+		<div class="row">
+		<div class="col-xs-12" style="background-color:#141414; height:2vw"></div>
+		</div>
+		<br>
 
-  <!-- RESERVE BUTTON DIV -->
-  <div class="reserve">
-    <center>
-      <div class="row">
-        <div class="col-xs-12" style="color:#ddd395"><h2>Reserve Your Luxury Suite Now</h2></div>
-      </div>
-      <br>
-      <br>
-      <br>
-      <div class="row">
-        <div class="col-xs-12"><img src="assets/img/line.svg" width="50%"></div>
-      </div>
-      <br>
-      <br>
-      <br>
-      <div class="row">
-        <div class="col-xs-12"><button type="button" class="btn btn-primary btn-md">ONLINE RESERVATION</button></div>
-       </div>
-    </center>
-  </div>
+		<div class="row" style="width:75%">
+		<div class="col-md-4"><img src="assets/img/home2.jpg" width="100%"></div>
+		<div class="col-md-8" style="text-align:left">
+			<h3>Lorem Ipsum</h3><br>
+			<p style="color:white;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+			Fusce mollis, erat in viverra luctus, mi lectus eleifend mauris, et vehicula lacus mi vitae sem. 
+			Phasellus scelerisque mauris ex, sit amet faucibus metus ornare et. Etiam vestibulum odio quis tincidunt aliquet. 
+			Sed sit amet ante at elit tempor maximus. Donec hendrerit vitae libero in rutrum. 
+			Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis a risus libero.</p>
+			<button type="button" class="btn btn-primary btn-md">READ MORE</button>
+		</div>
+		</div>
+		</center>
+		<br>
+		<br>
+	</div>
+
+	<!-- RESERVE BUTTON DIV -->
+	<div class="reserve">
+		<center>
+		<div class="row">
+			<div class="col-xs-12" style="color:#ddd395"><h2>Reserve Your Luxury Suite Now</h2></div>
+		</div>
+		<br>
+		<br>
+		<br>
+		<div class="row">
+			<div class="col-xs-12"><img src="assets/img/line.svg" width="50%"></div>
+		</div>
+		<br>
+		<br>
+		<br>
+		<div class="row">
+			<div class="col-xs-12"><button type="button" class="btn btn-primary btn-md">ONLINE RESERVATION</button></div>
+		</div>
+		</center>
+	</div>
 
 </div>
--- application/views/templates/Header.php
+++ application/views/templates/Header.php
@@ -1,64 +1,55 @@
 <html>
-<head>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>GrandABE Hotel</title>
-	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
-	<link rel = "stylesheet" type = "text/css" href = "assets/css/style.css">
+		<title>GrandABE Hotel</title>
+		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/style.min.css">
 
-	<script type="text/javascript" src="assets/js/jquery-3.1.0.min.js"></script>
-	<script src="assets/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="assets/js/jquery-3.1.0.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+	</head>
 
+	<body> 
 
-	<style>
+	<nav class="navbar navbar-fixed-top">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#ABENavbar">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand visible-xs" href="#">GrandABE Hotel</a>
+			</div>
+			<div class="collapse navbar-collapse" id="ABENavbar">
+				<ul class="nav navbar-nav col-sm-5">
+					<li class="leftNav"><a href="#"><h4>Rooms</h4></a></li>
+					<li class="dropdown leftNav">
+						<a class="dropdown-toggle" data-toggle="dropdown" href="#"><h4><img src="assets/img/ui/arrow.png" class="dropdown-img">The Hotel</h4></a>
+						<ul class="dropdown-menu dropdown-menu-right">
+							<li><a href="#">About GrandABE</a></li>
+							<li><a href="#">Our Philosophy</a></li>
+						</ul>
+					</li>
+				</ul>
+				<div class="nav navbar-nav col-sm-2 topbar-brand-logo hidden-xs">
+					<a href="#" style="padding:0"><center><img src="assets/img/top_logo.png"></center></a>
+				</div>
+				<ul class="nav navbar-nav col-sm-5 ">
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" href="#"><h4><img src="assets/img/ui/arrow.png" class="dropdown-img">Services</h4></a>
+						<ul class="dropdown-menu dropdown-menu-right">
+							<li><a href="#">Business & Meeting</a></li>
+							<li><a href="#">Facilities</a></li>
+							<li><a href="#">Packages</a></li>
+						</ul>
+					</li>
+					<li><a href="#"><h4>Contact</h4></a></li> 
+				</ul>
+				
+			</div>
+		</div>
+	</nav>
 
-
-
-  </style>
-
-</head>
-<body>
-
-<nav class="navbar navbar-fixed-top">
-  <div class="container-fluid">
-  	<div class="navbar-header">
-  	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#ABENavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-    </button>
-    <a class="navbar-brand visible-xs" href="#">GrandABE Hotel</a>
-    </div>
-	  <div class="collapse navbar-collapse" id="ABENavbar">
-	    <ul class="nav navbar-nav col-sm-5">
-	      <li class="dropdown leftNav">
-	      	<a class="dropdown-toggle" data-toggle="dropdown" href="#"><h4><span class="caret"></span>The Hotel
-	      	 </h4></a>
-	      	<ul class="dropdown-menu dropdown-menu-right">
-	          <li><a href="#">About GrandABE</a></li>
-	          <li><a href="#">Our Philosophy</a></li>
-	        </ul>
-	      </li>
-	      <li class="dropdown leftNav">
-	        <a class="dropdown-toggle" data-toggle="dropdown" href="#"><h4><span class="caret"></span>Rooms
-	        </h4></a>
-	        <ul class="dropdown-menu">
-	          <li><a href="#">Test1</a></li>
-	          <li><a href="#">Test2</a></li>
-	          <li><a href="#">Test3</a></li> 
-	        </ul>
-	      </li>
-	  	</ul>
-		<div class="nav navbar-nav col-sm-2 hidden-xs">
-	  		<a href="#" style="padding:0"><center><img src="assets/img/top_logo.png" width="95%"></center></a>
-	  	</div>
-	  	<ul class="nav navbar-nav col-sm-5 ">
-	      <li><a href="#"><h4>Reservations</h4></a></li>
-	      <li><a href="#"><h4>Contact</h4></a></li> 
-	   	</ul>
-	   	
-	  </div>
-  </div>
-</nav>
-
-<div style="height:3vh; width: 100%"></div> <!-- CHANGE HEIGHT -->
+	<div style="height:3vh; width: 100%"></div> <!-- CHANGE HEIGHT -->
